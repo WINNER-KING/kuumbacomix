787a2ddc31ae39e4f6f6762aff67210366e0cbf4
WINNER-KING <86776788+WINNER-KING@users.noreply.github.com>
2023-08-04 21:27:30 +0200

    commit by WK of 04-08-2022 at  20h25

--- kumba/kumba/settings.py
+++ kumba/kumba/settings.py
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr-fr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'UTC+1'
 
 USE_I18N = True
 
@@ -121,3 +121,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Authentification
+AUTH_USER_MODEL = 'authentication.User'
